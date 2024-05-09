484bf98f35e172ad04a2604bf126f14efa006be4
Viktor Kraft <viktor_kraft@hotmail.com>
2024-05-09 13:04:53 +0200

    OAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+
+  const { data: session } = useSession();
+
+  if (session) {
+    return /*redirect('/app')*/(
+      <div>
+        <p>Name: {session.user?.name}</p>
+        <p>Email: {session.user?.email}</p>
+        <img src={session.user?.image ?? ""} />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    );
+  }
+
   return (
-    <p>Hello, world!</p>
+    <div className="flex justify-center mt-16">
+      <div className="flex flex-col justify-center text-center gap-4">
+        <p>You are not logged in!</p>
+        <button onClick={async () => signIn('github')} className="bg-orange-500 hover:bg-orange-600 p-2 text-white rounded-xl">Sign In</button>
+      </div>
+    </div>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Navigation () {
   return (
     <>
-    
+      <Link href={"/"}>Workout</Link>
+      <Link href={"/"}>History</Link>
+      <Link href={"/"}>Statistics</Link>
     </>
   );
 }
