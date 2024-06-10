791ac805cbb1393cee9c27ba6195b681575f984f
Viktor Kraft <viktor_kraft@hotmail.com>
2024-06-10 12:44:00 +0200

    Fixed date display

--- src/components/Workout.tsx
+++ src/components/Workout.tsx
@@ -1,15 +1,28 @@
 
 export default function Workout ({ workout }) {
 
+  Number.prototype.padLeft = function(base,chr) {
+    var  len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+  }
+  
+  var date = new Date(workout.dateOfWorkout),
+      dateFormat =  [(date.getMonth()).padLeft(), 
+                      date.getDate().padLeft(),
+                      date.getFullYear()].join('/') + ' ' +
+                    [ date.getHours().padLeft(),
+                      date.getMinutes().padLeft(),
+                      date.getSeconds().padLeft()].join(':');
+                      
   return (
     <div className="flex flex-col bg-gray-900 rounded-lg m-2 p-4">
       <h2 className="font-bold text-xl text-orange-300">{workout.name}</h2>
-      <p>{workout.dateOfWorkout}</p>
+      <p>{dateFormat}</p>
       <div className="flex justify-between">
         <p>{workout.distance} meters,</p>
-        <p>time goal of {Math.floor(workout.timeGoal/6000)}:{Math.floor((workout.timeGoal%6000)/100)}</p>
+        <p>time goal of {(Math.floor(workout.timeGoal/6000)).padLeft()}:{(Math.floor((workout.timeGoal%6000)/100)).padLeft()}</p>
       </div>
-      <p>Finish time of {Math.floor(workout.compleationTime/6000)}:{Math.floor((workout.compleationTime%6000)/100)}:{(workout.compleationTime%6000)%100}</p>
+      <p>Finish time of {(Math.floor(workout.compleationTime/6000)).padLeft()}:{(Math.floor((workout.compleationTime%6000)/100)).padLeft()}:{((workout.compleationTime%6000)%100).padLeft()}</p>
     </div>
   );
 }
